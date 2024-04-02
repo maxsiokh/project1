7614660863d630a0125c995fa635868ff4edaf7d
maxsiokh <maxsiokh@gmail.com>
2024-04-02 17:48:22 +0300

    yea

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('.services__inner').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeIn");
+	        }
+	    });
+	});
+}
